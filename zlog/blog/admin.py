f423414d5d8fe8e58f27eacc4b132203e324e8bf
Hessam <hessam.hashemi21@gmail.com>
2019-12-16 17:58:57 +0330

    completed the notebook -before session

--- zlog/blog/admin.py
+++ zlog/blog/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
-
+from .models import BlogPost
 # Register your models here.
+
+
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'title',
+        'overview',
+        'created_at',
+        'updated_at',
+        'creator',
+        'target_age'
+    )
+    search_fields = (
+        'text',
+        'title',
+        'overview'
+    )
+    readonly_fields = (
+        'created_at',
+        'updated_at'
+    )
+    list_filter = (
+        'target_age',
+        'creator'
+    )
+
+
+admin.site.register(BlogPost, BlogPostAdmin)
